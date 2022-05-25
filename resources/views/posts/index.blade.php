82c8721e31ee570d00a6226f74583be3edea013b
mmcoco72 <bad.ema.1011@gmail.com>
2022-05-25 23:34:22 +0900

    08-4 課題3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,8 +7,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <p class='create'>[<a href='.posts/create'>create</a>]</p>
+    　<h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
